62a92821bc0ef1389da0791f43c547d2c6435847
Cthede11 <147101801+Cthede11@users.noreply.github.com>
2024-02-14 12:59:25 -0600

    testing folder
    added a test folder and implemented a "past pages" section beside the scape and randomize section that allows the user to go back to a previous page

--- testing zone/ScraperTest.py
+++ testing zone/ScraperTest.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from bs4 import BeautifulSoup
 import requests
 import random
 
 app = Flask(__name__)
+app.secret_key = '1234ABCD'
 current_page = ''
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,22 +12,26 @@ def index():
     global current_page
             
     if request.method == 'POST':
-            if 'randomize' in request.form:  # "Randomize" button
-                links, _ = get_links(current_page)
-                random_link = random.choice(links) if links else ''
-                current_page = random_link
+        if 'randomize' in request.form:  # "Randomize" button
+            links, _ = get_links(current_page)
+            random_link = random.choice(links) if links else ''
+            current_page = random_link
                 
-            else:
-                current_page = request.form.get('url')
+        else:
+            current_page = request.form.get('url')
 
     # Fetch title after setting the new current_page
     if current_page:
         _, title = get_links(current_page)
-
+        # Update the list of past pages
+        past_pages = session.get('past_pages', [])
+        past_pages.append(current_page)
+        session['past_pages'] = past_pages[-5:]  # Keep only the last 5 pages
     else:  # Watch for Empty URL
-        title = ''
+        title = 'Unknown or Missing Page Title'
 
-    return render_template('index.html', link=current_page, title=title)
+    past_pages = session.get('past_pages', [])
+    return render_template('index.html', link=current_page, title=title, past_pages=past_pages)
 
 def get_links(url):
     response = requests.get(url)
